4a75e6680e74ec1db721364a17a2e32faf7ef469
Samuel Sampaio <samuelsd@dcc.ufrj.br>
2023-08-07 22:32:54 -0300

    botão de incrementar e decrementar na lista

--- script.js
+++ script.js
@@ -18,31 +18,64 @@ setInterval(nextSlide, 3000);
 
 
 let carrinho = [];
-let total = 0;
+        let total = 0;
 
-function adicionarAoCarrinho(nome, preco) {
-    if (carrinho.indexOf(nome) === -1) {
-        carrinho.push(nome);
-        total += preco;
-        document.getElementById("itens-carrinho-nav").innerHTML += `<li>${nome} - R$${preco}</li>`;
-        document.getElementById("total-nav").innerText = total;
-        document.querySelector(".carrinho-contador").innerText = carrinho.length;
-    } else {
-        alert("Este item já está no carrinho!");
-    }
-}
+        function adicionarAoCarrinho(nome, preco) {
+            if (carrinho.some(item => item.nome === nome)) {
+                alert("Este item já está no carrinho!");
+                return;
+            }
 
-function mostrarCarrinho() {
-    const carrinhoLista = document.getElementById("carrinho-lista");
-    const listaProdutos = document.getElementById("lista-produtos");
-    
-    if (carrinhoLista.style.display == "block") {
-        carrinhoLista.style.display = "none";
-        listaProdutos.style.display = "grid";
-    } else {
-        carrinhoLista.style.display = "block";
-        listaProdutos.style.display = "none";
-    }
-}
+            carrinho.push({ nome: nome, preco: preco, quantidade: 1 });
+            total += preco;
+
+            const itensCarrinhoNav = document.getElementById("itens-carrinho-nav");
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `${nome} - R$${preco} - Quantidade: ${carrinho[carrinho.length - 1].quantidade} <button onclick="incrementarQuantidade(${carrinho.length - 1}, ${preco})">+</button> <button onclick="decrementarQuantidade(${carrinho.length - 1}, ${preco})">-</button>`;
+            itensCarrinhoNav.appendChild(listItem);
+
+            document.getElementById("total-nav").innerText = total;
+            document.querySelector(".carrinho-contador").innerText = carrinho.length;
+        }
+
+        function incrementarQuantidade(index, preco) {
+            carrinho[index].quantidade++;
+            total += preco;
+            atualizarListaCarrinho();
+        }
+
+        function decrementarQuantidade(index, preco) {
+            if (carrinho[index].quantidade > 1) {
+                carrinho[index].quantidade--;
+                total -= preco;
+                atualizarListaCarrinho();
+            }
+        }
+
+        function atualizarListaCarrinho() {
+            const itensCarrinhoNav = document.getElementById("itens-carrinho-nav");
+            itensCarrinhoNav.innerHTML = "";
+
+            carrinho.forEach((item, index) => {
+                const listItem = document.createElement("li");
+                listItem.innerHTML = `${item.nome} - R$${item.preco} - Quantidade: ${item.quantidade} <button onclick="incrementarQuantidade(${index}, ${item.preco})">+</button> <button onclick="decrementarQuantidade(${index}, ${item.preco})">-</button>`;
+                itensCarrinhoNav.appendChild(listItem);
+            });
+
+            document.getElementById("total-nav").innerText = total;
+        }
+
+        function mostrarCarrinho() {
+            const carrinhoLista = document.getElementById("carrinho-lista");
+            const listaProdutos = document.getElementById("lista-produtos");
+            
+            if (carrinhoLista.style.display === "block") {
+                carrinhoLista.style.display = "none";
+                listaProdutos.style.display = "block";
+            } else {
+                carrinhoLista.style.display = "block";
+                listaProdutos.style.display = "none";
+            }
+        }
 
 
